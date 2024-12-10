bcde807237a5f88cd11897fd70275f2d249d60c9
Huynh Linh Hoai <22130084@st.hcmuaf.edu.vn>
2024-12-10 21:49:56 +0700

    finish task of hoai

---
+++ fontend/js/products.js
@@ -0,0 +1,15 @@
+
+    // Lấy tất cả các thẻ card
+    const cards = document.querySelectorAll('.card');
+
+    // Lặp qua từng card và thêm sự kiện click
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            // Lấy URL từ thuộc tính data-url
+            const url = card.getAttribute('data-url');
+            if (url) {
+                // Chuyển hướng đến URL
+                window.location.href = url;
+            }
+        });
+    });
