9f5f97cae87acc5cea0acacee8eca1897d1358d5
Huynh Linh Hoai <22130084@st.hcmuaf.edu.vn>
2025-01-04 09:54:51 +0700

    asp.net

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectDotNET.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}
