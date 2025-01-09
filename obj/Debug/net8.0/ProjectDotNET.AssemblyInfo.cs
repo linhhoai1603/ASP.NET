f3c2b778cbe279789c4d004d9628bba7d7d90194
Huynh Linh Hoai <22130084@st.hcmuaf.edu.vn>
2025-01-09 12:13:53 +0700

    fix conflict

--- obj/Debug/net8.0/ProjectDotNET.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjectDotNET.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectDotNET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b74c028023ed27710766173e97ec2898a899d3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b2a1b2f4282f32f84b9d37ac132f261d0fa66d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectDotNET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectDotNET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
