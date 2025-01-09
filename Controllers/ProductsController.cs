7dc9cf0b706445bc4d8b1b4477590756caf8effb
UMI <22130069@st.hcmuaf.edu.vn>
2025-01-09 19:03:14 +0700

    pull models

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,15 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjectDotNET.Models;
 
 namespace ProjectDotNET.Controllers
 {
     public class ProductsController : Controller
     {
-        public IActionResult Index()
+
+        private readonly Shop_Context _context;
+
+        public ProductsController(Shop_Context context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Index()
         {
             return View();
         }
         [HttpGet]
-        public IActionResult Products()
+        public ActionResult Products()
         {
             return View();
         }
